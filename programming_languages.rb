67ab035f94b439e0309e577c9e86536b465f6cdf
Shawn Ren <doggy-ren@hotmail.com>
2019-03-20 20:13:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,14 +1,10 @@
 def reformat_languages(languages)
   new_languages = {}
-  languages.each do |style, language|
-    language.each do |language_name, attribute|
-      if new_languages[language_name] == nil
-          new_languages[language_name] = attribute
-        end
-      new_languages[language_name][:style] = []
-      new_languages[language_name][:style] << style
+  languages.each do |style, language_hash|
+    language_hash.each do |language_name, attributes|
+      new_languages[language_name] = attributes
     end
+    new_languages[language_name][:style] = style
   end
-  new_languages[:javascript][:style] << :oo
   new_languages
 end
